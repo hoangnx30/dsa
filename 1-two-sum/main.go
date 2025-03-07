b4812fa42f200be8495055f68b7bbfdea4363022
Hoang Nguyen <xuanhoang30071999@gmail.com>
2025-03-07 22:38:25 +0700

    [two-sum] brute force solution

--- 1-two-sum/main.go
+++ 1-two-sum/main.go
@@ -13,9 +13,22 @@ func hashMapSolution(nums []int, target int) []int {
 	return nil
 }
 
+func bruteForce(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	var nums = []int{1, 2, 4, 6, 7}
 	var target = 6
 	fmt.Println(hashMapSolution(nums, target))
+	fmt.Println(bruteForce(nums, target))
 
 }
